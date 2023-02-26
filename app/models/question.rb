f410cea1c70c60c9b4896ccf391a5411e9554c24
Nitek <nitekkumarsingh@gmail.com>
2023-02-26 12:45:54 +0530

    Questions Validation Added

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,4 +2,13 @@ class Question < ApplicationRecord
   belongs_to :exam
 
   validates :question, :answer, :option_1, :option_2, :option_3, :exam_id, presence: true
+  validate :answer_in_options
+
+  private
+
+  def answer_in_options
+    unless [option_1, option_2, option_3].include?(answer)
+      errors.add(:answer, "must match one of the options")
+    end
+  end
 end
