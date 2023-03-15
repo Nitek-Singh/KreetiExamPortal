a7ed96cd86c2a76217cf0b1cfbb5956e332a9ff6
Nitek <nitekkumarsingh@gmail.com>
2023-03-15 11:00:16 +0530

    Pop_up Message Added While Deletion

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,12 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import 'bootstrap'
+
+
+function confirmDelete(event) {
+  event.preventDefault(); // prevent the default form submission
+  if (confirm("Are you sure you want to delete this?")) {
+    event.target.closest("form").submit(); // submit the form
+  }
+}
+
